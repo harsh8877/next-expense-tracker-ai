16b44feb02961baef0e0ed916fcd2854d9d1767f
harsh8877 <vasoyaharsh123@gmail.com>
2026-02-23 22:23:39 +0530

    changes for next config

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,16 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: (ReturnType<typeof createPrismaClient>) | undefined;
+  var prisma: ReturnType<typeof createPrismaClient> | undefined;
 }
 
 const createPrismaClient = () => {
   return new PrismaClient().$extends({
     model: {
       user: {
-        async findUser(args: { where: { clerkUserId: string } }) {
-          return (this as any).findUnique(args);
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        async findUser(this: any, args: { where: { clerkUserId: string } }) {
+          return this.findUnique(args);
         },
       },
     },
